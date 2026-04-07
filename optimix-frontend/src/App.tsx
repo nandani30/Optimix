9d444bbb6af5f658696ca49e6d002132a219f9c8
Nandani <nandani@Nandanis-MacBook-Air.local>
2026-04-07 19:30:16 +0530

    Fix: Switched to HashRouter and updated Vite base path to fix Electron blank screen

--- optimix-frontend/src/App.tsx
+++ optimix-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthStore } from './store'
 import ErrorBoundary from './components/shared/ErrorBoundary'
 import AppLayout from './components/layout/AppLayout'
@@ -18,7 +18,7 @@ export default function App() {
 
   return (
     <ErrorBoundary>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           {/* Public */}
           <Route
@@ -45,7 +45,7 @@ export default function App() {
           {/* Catch-all */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ErrorBoundary>
   )
 }
